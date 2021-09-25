b5bd9707f24b9a3f78e1ca0a7ac777e6bc68d7a3
Elsaeed Ahmed <elsaeedahmed97@gmail.com>
2021-09-25 14:51:00 +0200

    add CRUD for jobs

--- backend/apis/v1/route_job.py
+++ backend/apis/v1/route_job.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, session
+from sqlalchemy.sql.expression import delete, update
 from db.session import get_db
-from db.repository.job import create_new_job
+from db.repository.job import create_new_job, retrieve_job, list_jobs, update_job, delete_job
 from db.schemas.jobs import JobCreate, JobShow
 
 router = APIRouter()
@@ -12,3 +13,38 @@ def create_job(job: JobCreate, db: Session = Depends(get_db)):
     owner_id = 1
     job = create_new_job(job=job, db=db, owner_id=owner_id)
     return job
+
+
+@router.get("/job/{id}", response_model=JobShow)
+def retrieve_job_by_id(id: int, db: Session = Depends(get_db)):
+    job = retrieve_job(id=id, db=db)
+    if not job:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Job with id {id} does'nt exists")
+    return job
+
+
+@router.get("/all")
+def list_all_jobs(db: Session = Depends(get_db)):
+    jobs = list_jobs(db)
+    return jobs
+
+
+@router.put("/update/{id}")
+def update_job_by_id(id: int, job: JobCreate, db: Session = Depends(get_db)):
+    owner_id = 1
+    job_to_update = update_job(id=id, job=job, db=db, owner_id=owner_id)
+    if not job_to_update:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Job with id {id} does'nt exists")
+    return {"details": "Successfully updated"}
+
+
+@router.delete("delete/{id}")
+def delete_job_by_id(id: int, db: Session = Depends(get_db)):
+    owner_id = 1
+    job_to_delete = delete_job(id=id, db=db, owner_id=owner_id)
+    if not job_to_delete:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Job with id {id} does'nt exists")
+    return {"details": "Successfully Deleted"}
--- backend/db/repository/job.py
+++ backend/db/repository/job.py
@@ -1,3 +1,5 @@
+from fastapi.exceptions import HTTPException
+from starlette.status import HTTP_404_NOT_FOUND
 from db.models.jobs import Job
 from db.schemas.jobs import JobCreate
 from sqlalchemy.orm import Session
@@ -9,3 +11,32 @@ def create_new_job(job: JobCreate, db: Session, owner_id: int):
     db.commit()
     db.refresh(job)
     return job
+
+
+def retrieve_job(id: int, db: Session):
+    job = db.query(Job).filter(Job.id == id).first()
+    return job
+
+
+def list_jobs(db: Session):
+    jobs = db.query(Job).filter(Job.is_active == True).all()
+    return jobs
+
+
+def update_job(id: int, job: JobCreate, db: Session, owner_id: int):
+    existing_job = db.query(Job).filter(Job.id == id)
+    if not existing_job.first():
+        return 0
+    job.__dict__.update(owner_id=owner_id)
+    existing_job.update(job.__dict__)
+    db.commit()
+    return 1
+
+
+def delete_job(id: int, db: Session, owner_id: int):
+    existing_job = db.query(Job).filter(Job.id == id)
+    if not existing_job.first():
+        return 0
+    existing_job.delete(synchronize_session=False)
+    db.commit()
+    return 1
